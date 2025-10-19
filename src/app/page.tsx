6a2504e3e3bd84e14ce9137bc91aa2e8f4bebaec
Liuber <lfornarisr@gmail.com>
2025-10-19 20:51:15 -0300

    navbar ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import Hero_Section from "@/components/HeroSection";
+import ExperienceSection from "@/components/ExperienceSection";
+import AboutSection from "@/components/AboutSection";
+import ContactSection from "@/components/ContactSection";
 export default function Home() {
   return (
     <>
       <main>
         <Hero_Section />
+        <ExperienceSection />
+        <AboutSection />
+        <ContactSection />
       </main>
     </>
   );
