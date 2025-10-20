2eb9cfa3d2bff69f5ca94925ff3483f6d3316ea4
lfornarisr <126205502+lfornarisr@users.noreply.github.com>
2025-10-20 00:06:48 -0300

    Dev (#7)
    * fix(light-mode): corrige contraste y fondos claros (#5)
    
    * fix(light-mode): corrige contraste y fondos claros (#6)

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import Button from "./Button";
 import FadeInSection from "./FadeInSection";
 
 const HeroSection = () => {
   return (
-    <section id="sobremi" className="h-screen">
+    <section id="sobremi" className="">
       <FadeInSection>
         <div className="h-full flex flex-col items-center justify-center space-y-8">
           <h2 className="text-4xl">Sobre mí</h2>
