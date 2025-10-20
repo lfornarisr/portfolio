bc43858dc7afa21cdff6c29eb6115e162a8599de
lfornarisr <126205502+lfornarisr@users.noreply.github.com>
2025-10-19 21:13:53 -0300

    navbar ready (#3) (#4)

---
+++ src/components/ExperienceSection.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Button from "./Button";
+import FadeInSection from "./FadeInSection";
+
+const HeroSection = () => {
+  return (
+    <section id="experiencia" className="h-screen">
+      <FadeInSection>
+        <div className="h-full flex flex-col items-center justify-center space-y-8">
+          <h2 className="text-4xl">Experiencia</h2>
+        </div>
+      </FadeInSection>
+    </section>
+  );
+};
+
+export default HeroSection;
